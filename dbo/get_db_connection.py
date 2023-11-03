f05db258085e59b68f2412d43447fa2ca867254d
ackatz <andrew@akatz.org>
2023-11-02 18:08:57 -0600

    Fix db conn and add security updates

--- dbo/get_db_connection.py
+++ dbo/get_db_connection.py
@@ -2,7 +2,7 @@ import sqlite3
 
 
 def get_db_connection():
-    conn = sqlite3.connect("/app/whisper_app.db")
+    conn = sqlite3.connect("/litefs/whisper_app.db")
     conn.execute("pragma journal_mode=wal")
     conn.row_factory = sqlite3.Row
     return conn
